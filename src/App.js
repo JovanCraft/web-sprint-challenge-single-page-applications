8f466c1b34f0dd66d1cfd057be6731bee4285e49
Jovan Craft <jovancraft@gmail.com>
2023-09-02 10:25:11 -0500

    wrapped my App compnent in index.js correctly this time

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
-    </BrowserRouter>
+    </>
   );
 };
 export default App;
