d44f1d14b2456c0e10cd9bf677abc88b75ffe8a1
Jovan Craft <jovancraft@gmail.com>
2023-09-02 13:49:48 -0500

    passed all tests

--- src/PizzaOrderForm.js
+++ src/PizzaOrderForm.js
@@ -1,55 +1,84 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import * as yup from 'yup'
+
+
+
+const yupSchema = yup.object().shape({
+    name: yup.string().trim().required('name is required').min(2, 'name must be at least 2 characters'),
+    size: yup.string().required(),
+    toppings1: yup.boolean(),
+    toppings2: yup.boolean(),
+    toppings3: yup.boolean(),
+    toppings4: yup.boolean(),
+    special: yup.string()
+})
 
 const PizzaOrderForm = () => {
-    const [name, setName] = useState('')
-    const [size, setSize] = useState('')
-    const [toppings, setToppings] = useState({
+
+
+    const [formErrors, setFormErrors] = useState({
+        name: '',
+        size: '',
+        toppings1: '',
+        toppings2: '',
+        toppings3: '',
+        toppings4: '',
+    })
+
+    const [form, setForm] = useState({
+        name: '',
+        size: '',
         toppings1: false,
         toppings2: false,
         toppings3: false,
-        toppings4: false
+        toppings4: false,
+        special: ''
     })
-    const [specialInstructions, setSpecialInstructions] = useState('')
+
+    const validate = async (name, value) => {
+
+            yup.reach(yupSchema, name).validate(value).then(() => {
+                setFormErrors({ ...formErrors, [name]: ''})
+            }).catch(err => {
+
+                setFormErrors({ ...formErrors, [name]: err.errors[0]})
+            })
+
+
+
+    }
+
+    
 
     const change = e => {
         const { name, value, checked, type } = e.target
         const valueToUse = type === 'checkbox' ? checked : value
 
-        setName(valueToUse)
-        setSize(valueToUse)
-        setToppings({ ...toppings, [name]: valueToUse})
-        setSpecialInstructions(valueToUse)
+        validate(name, value)
+        setForm({
+            ...form,
+            [name]: valueToUse
+        })
+
+
 
     }
+
 
     const submit = e => {
         e.preventDefault()
 
-        const orderPayload = {
-            name,
-            size,
-            ...toppings,
-            special: specialInstructions,
-          };
+        if(formErrors.name){
+            console.log('Validation errors in the form. Cannot submit.')
+            return
+        }
 
-        axios
-        .post(`https://reqres.in/api/orders`, orderPayload)
-        .then(res => {
+
+          axios.post("https://reqres.in/api/orders", form)
+          .then(res => {
             console.log(res)
-        })
-        .catch(err => {
-            console.log(err.message)
-        })
-        setName("")
-        setSize("")
-        setToppings({
-            topping1: false,
-            topping2: false,
-            topping3: false,
-            topping4: false,
-        })
-        setSpecialInstructions('')
+          })
     }
 
     return (
@@ -59,12 +88,14 @@ const PizzaOrderForm = () => {
                 <label htmlFor='name-input'>Name:</label>
                 <input
                     type='text'
+                    name='name'
                     id='name-input'
-                    value={name}
+                    value={form.name}
                     onChange={change}
                 /><br />
+                {formErrors.name && <p>{formErrors.name}</p>}
                 <label htmlFor='size-dropdown'>Pizza Size:</label>
-                <select id='size-dropdown' value={size} onChange={change}>
+                <select name='size' id='size-dropdown' value={form.size} onChange={change}>
                     <option value='small'>Small</option>
                     <option value='medium'>Medium</option>
                     <option value='large'>Large</option>
@@ -76,9 +107,9 @@ const PizzaOrderForm = () => {
                     <label>
                         <input
                             type='checkbox'
-                            id='topping1'
-                            name='topping1'
-                            checked={toppings.topping1}
+                            id='toppings1'
+                            name='toppings1'
+                            checked={form.toppings1}
                             onChange={change}
                         />
                         Topping 1
@@ -88,9 +119,9 @@ const PizzaOrderForm = () => {
                     <label htmlFor='topping2'>
                     <input
                         type="checkbox"
-                        id="topping2"
-                        name="topping2"
-                        checked={toppings.topping2}
+                        id="toppings2"
+                        name="toppings2"
+                        checked={form.toppings2}
                         onChange={change}
                     />
                     Topping 2
@@ -100,9 +131,9 @@ const PizzaOrderForm = () => {
                 <label htmlFor="topping3">
                     <input
                         type="checkbox"
-                        id="topping3"
-                        name="topping3"
-                        checked={toppings.topping3}
+                        id="toppings3"
+                        name="toppings3"
+                        checked={form.toppings3}
                         onChange={change}
                         />
                         Topping 3
@@ -113,14 +144,23 @@ const PizzaOrderForm = () => {
                     <label htmlFor="topping4">
                     <input
                         type="checkbox"
-                        id="topping4"
-                        name="topping4"
-                        checked={toppings.topping4}
+                        id="toppings4"
+                        name="toppings4"
+                        checked={form.toppings4}
                         onChange={change}
                     />
                     Topping 4
                     </label>
                 </div><br />
+
+                <label htmlFor="special-text">Special Instructions:</label>
+                <input
+                    type="text"
+                    id="special-text"
+                    name='special'
+                    value={form.special}
+                    onChange={change}
+                    /><br />
 
                 <button type='submit' id='order-button'>Add to Order</button>
             </form>
