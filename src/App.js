3f3162412f43921dbc6db4e811f2b63f71ff0d92
Jovan Craft <jovancraft@gmail.com>
2023-09-02 10:39:20 -0500

    got three passing in the test specs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import HomePage from './HomePage'
 
 const App = () => {
   return (
-    <>
+    <div>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
-    </>
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+      </Routes>
+    </div>
   );
 };
 export default App;
---
+++ src/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function HomePage() {
+    return (
+        <div>
+            <h2>Welcome to Bloomtech Eats</h2>
+            <p>Order your custom pizza now!</p>
+            <Link to='pizza' id='order-pizza'>Order Pizza</Link>
+        </div>
+    )
+}
